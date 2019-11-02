8f4220269e143303e88647293f96b500e67be299
Jay <jram.snh@gmail.com>
2019-11-02 14:59:55 +0530

    Initial Commit

---
+++ hello.py
@@ -0,0 +1,22 @@
+import math
+import os
+import sys
+
+import requests
+
+print(sys.version)
+print(sys.executable)
+
+
+def greet(who_to_gret):
+    greeting = 'Hello, {}'.format(who_to_gret)
+    return greeting
+
+
+print(greet('World'))
+print(greet('Corey'))
+r = requests.get("http://www.google.com")
+print(r.status_code)
+
+name = input("Enter your name")
+print("Hello, " + name)
